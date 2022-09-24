9e561d9ea35333fd75ff15c9378a95ea4a852e01
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-09-24 18:57:45 +0300

    javascript --degisken-türleri oluşturuldu.

---
+++ degisken-turleri.js
@@ -0,0 +1,15 @@
+var urunAdi = "iphone 13";
+let urunFiyat = 15000; 
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = "10";
+let sayi2 = "20";
+
+console.log(Number(sayi1)+ Number(sayi2));
+
+let sayi3 = 10;
+let sayi4 = 20;
+
+console.log(sayi3.toString() + sayi4.toString());
