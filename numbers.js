ba622ee4730a07e9dd3c77fddc946790364500f3
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-04-26 16:01:01 +0300

    numbers oluşturuldu.

---
+++ numbers.js
@@ -0,0 +1,15 @@
+let sonuc;
+
+sonuc = 10;
+sonuc = "10";
+
+sonuc = Number(sonuc);
+sonuc = parseInt("10.6");
+sonuc = parseFloat("10.32")
+
+sonuc = sonuc.toFixed(1);
+sonuc = Math.round(10.3);
+
+
+console.log(sonuc);
+console.log(typeof sonuc);
