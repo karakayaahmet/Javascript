c243128b372f64ee335992a887de4a2cb0bbf17a
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-04-26 18:37:11 +0300

    diziler.js oluşturuldu.

---
+++ diziler.js
@@ -0,0 +1,23 @@
+let urun1 = "iphone 11";
+let urun2 = "iphone 13";
+let urun3 = "iphone 13 pro";
+
+let urunler = ["iphone 11","iphone 13","iphone 13 pro"];
+let fiyatlar = [5000,7000,10000];
+let renkler = ["gold","siyah","beyaz"];
+
+console.log(urunler[0]);
+console.log(urunler[1]);
+console.log(urunler[2]);
+
+console.log(`${urunler[0]}--${fiyatlar[0]}--${renkler[0]}`);
+console.log(`${urunler[1]}--${fiyatlar[1]}--${renkler[1]}`);
+console.log(`${urunler[2]}--${fiyatlar[2]}--${renkler[2]}`);
+
+let kursAdi = "Komple Web Geliştirme Eğitimi";
+
+console.log(kursAdi[5]);
+console.log(kursAdi.split(" "));
+console.log(kursAdi.split(" ")[3]);
+
+
